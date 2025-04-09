08d8776a401bf29b703c6d63548d1f41df65cf82
Nasgull07 <pol.doria.rubio@estudiantat.upc.edu>
2025-04-09 10:42:41 +0200

    Version preparada para refresh token

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { Bb8ToggleComponent } from '../../UI/bb8-toggle/bb8-toggle.component';
+import { RedComponent } from '../../UI/buttons/red/red.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'navbar',
-  imports: [Bb8ToggleComponent],
+  imports: [Bb8ToggleComponent, RedComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -14,5 +16,13 @@ export class NavbarComponent {
     toggleTheme(event: any): void {
       this.isDarkTheme = !!event; // Actualiza el tema según el valor emitido por bb8-toggle
       this.themeChange.emit(this.isDarkTheme);
+    }
+
+    constructor(private router: Router) {}
+
+    logout(){
+      this.router.navigate(['/login']);
+      localStorage.removeItem('token'); // Elimina el token del localStorage
+      localStorage.removeItem('email'); // Elimina el usuario del localStorage
     }
 }
